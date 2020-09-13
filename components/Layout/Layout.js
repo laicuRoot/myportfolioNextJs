6d5f62aaba78e59e444f0afe2f53b3ce7f84f287
laicuRoot <jdblancoes@gmail.com>
2020-09-13 21:25:22 +0100

    updated layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from './Layout.module.css';
-import utilStyles from '../../styles/utils.module.css';
+// import utilStyles from '../../styles/utils.module.css';
 
 export const siteTitle = 'Jose Blanco Portfolio'
 
@@ -15,40 +15,43 @@ export default function Layout({ children, home }) {
 
       {home ? (
         <>
-          <section className={styles.avatarContainer}>
+          {/* <section className={styles.avatarContainer}>
             <img className={styles.avatar} src="../../images/profile.png" alt="avatar" />
-          </section>
+          </section> */}
+          <main>
+            <div className='container mx-auto px-5'>
+              <header className='flex-col md:flex-row flex items-center md:justify-between mt-5 mb-5 md:mb-3'>
+                <h1 className='text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8'> Front End Developer </h1>
+              </header>
 
-          <section className={styles.cardContainer}>
-            <h1 className={styles.title}> Front End Developer </h1>
-            <hr className={styles.hr} />
+              <hr />
 
-            <ul className={styles.skillsContainer}>
-              <li className={styles.skill}>HTML & CSS /</li>
-              <li className={styles.skill}>NPM /</li>
-              <li className={styles.skill}>JAVASCRIPT /</li>
-              <li className={styles.skill}>REACT /</li>
-              <li className={styles.skill}>NEXT.JS</li>
-            </ul>
+              <section className='container mx-auto'>
+                <div className='mb-4 text-lg leading-tight text-left px-4 py-2 m-2 h-40'>
+                  <p className='text-4xl'> Hello I'm Jose Blanco and I'm self-tough front-end developer</p>
+                </div>
+              </section>
 
-            <ul className={styles.skillsContainer}>
-              <li className={styles.icon}>
-                <i className="fab fa-github fa-5x"></i>
-              </li>
-              <li className={styles.icon}>
-                <i className="fab fa-js fa-5x"></i>
-              </li>
-              <li className={styles.icon}>
-                <i className="fab fa-npm fa-5x"></i>
-              </li>
-              <li className={styles.icon}>
-                <i className="fab fa-react fa-5x"></i>
-              </li>
-              <li className={styles.icon}>
-                <i className="fab fa-node fa-5x"></i>
-              </li>
-            </ul>
-          </section>
+              <section className='container mx-auto'>
+                
+                <div className='flex flex-row text-center flex-wrap p-2 space-x-2 w-full justify-center'>
+                  <div className='mb-4 text-lg leading-tight'><span>HTML & CSS /</span></div>
+                  <div className='mb-4 text-lg leading-tight'><span>JAVASCRIPT /</span></div>
+                  <div className='mb-4 text-lg leading-tight'><span>REACT /</span></div>
+                  <div className='mb-4 text-lg leading-tight'><span>NEXT.JS/</span></div>
+                  <div className='mb-4 text-lg leading-tight'><span>NPM </span></div>
+                </div>
+
+                <div className='flex flex-row text-center flex-wrap justify-items-center p-2 space-x-2 w-full justify-center'>
+                  <div className='mb-4 text-lg leading-tight'><i className="fab fa-github fa-5x"></i></div>
+                  <div className='mb-4 text-lg leading-tight'><i className="fab fa-js fa-5x"></i></div>
+                  <div className='mb-4 text-lg leading-tight'><i className="fab fa-npm fa-5x"></i></div>
+                  <div className='mb-4 text-lg leading-tight'><i className="fab fa-react fa-5x"></i></div>
+                  <div className='mb-4 text-lg leading-tight'><i className="fab fa-node fa-5x"></i></div>
+                </div>
+              </section>
+              </div>
+          </main>
         </>
       ) : (
         <>
--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -5,27 +5,25 @@ import navButtons from '../../pages/buttons';
 
 const NavButton = props => (
   <Link href={props.path} replace>
-    <li className={`${styles.item} block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4`}>{props.label}</li>
+    <div className={`${styles.item} block lg:inline-block lg:mt-0 text-black-200 hover:shadow-md mr-2 p-1 rounded hover:bg-gray-300`}><span className='text-sm lg:flex-grow'>{props.label}</span></div>
   </Link>
 );
 
 export default function Nav(){
   return (
     <nav className='flex items-center justify-between flex-wrap p-6'>
-      <div className='flex items-center flex-shrink-0 text-white mr-6'>
+      <div className='flex items-center flex-shrink-0 text-black mr-6'>
         <Link href='/'>
           <span className='font-semibold text-xl tracking-tight'>Jose Blanco</span>
         </Link>
       </div>
-      <ul className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
-        <div className='text-sm lg:flex-grow'>
+      <div className='flex justify-end w-full block lg:flex lg:items-center lg:w-auto'>
           {navButtons.map((button) => {
             return (
               <NavButton key={button} path={button.path} label={button.label} />
             );
           })}
-        </div>
-      </ul>
+      </div>
     </nav>
   );
 }
