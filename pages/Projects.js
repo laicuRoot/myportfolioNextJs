e25da6de447fc1fbf01c419f50124e5ae1f21527
laicuRoot <jdblancoes@gmail.com>
2020-10-17 21:06:01 +0100

    Modfied Grid

--- pages/Projects.js
+++ pages/Projects.js
@@ -2,46 +2,42 @@ import Layout from '../components/Layout/Layout'
 import projects from '../projects';
 import Card from '../components/Card/Card';
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import { GridListTileBar } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
-    overflow: 'hidden',
-    marginTop: '-4rem',
+    marginTop: '1rem',
     backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 1000,
-    height: 1000,
+    padding:'3rem'
   },
 }));
 
 
-const ProjectList = (props) =>{
+const ProjectList = ({projects}) =>{
   const classes = useStyles();
+
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={300} spacing={20} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={4} style={{ height: 'auto' }}>
-          <ListSubheader component="div"></ListSubheader>
-        </GridListTile>
-        {props.projects.map((project, id) => {
-          return (
-              <GridListTile key={id}>
-                <img src={project.photo}/>
-                <GridListTileBar 
-                  title={project.title}
-                />
-              </GridListTile>
-          );
-        })}
-      </GridList>
+    <div className='container mx-auto'>
+      <Grid 
+        container 
+        spacing={5}
+        className={`${classes.root} shadow-lg`}
+      >
+        {projects.map((project,id) =>
+          <Grid key={id} item
+            xs={12} sm={6} md={4} lg={4} xl={3}
+          >
+            <Card
+              key={id}
+              photo={project.photo}
+              description={project.description}
+              title={project.title}
+            />
+          </Grid>)}
+      </Grid>
     </div>
   );
 }
