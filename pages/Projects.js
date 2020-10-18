2a8c0120a3cb2ebca8eeadb808df05448098f7e6
laicuRoot <jdblancoes@gmail.com>
2020-10-18 19:31:21 +0100

    removed material UI - simplify with tailwind

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,43 +1,24 @@
 import Layout from '../components/Layout/Layout'
 import projects from '../projects';
-import Card from '../components/Card/Card';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid'
+import TailCard from '../components/TailCard/TailCard';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    marginTop: '1rem',
-    backgroundColor: theme.palette.background.paper,
-    padding:'3rem'
-  },
-}));
 
 
 const ProjectList = ({projects}) =>{
-  const classes = useStyles();
 
   return (
-    <div className='container mx-auto'>
-      <Grid 
-        container 
-        spacing={5}
-        className={`${classes.root} shadow-lg`}
-      >
-        {projects.map((project,id) =>
-          <Grid key={id} item
-            xs={12} sm={6} md={4} lg={4} xl={3}
-          >
-            <Card
-              key={id}
-              photo={project.photo}
-              description={project.description}
-              title={project.title}
-            />
-          </Grid>)}
-      </Grid>
+    <div className='container my-12 mx-auto px-4 md:px-12'>
+      <div className='block md:flex justify-between md:-mx-2'>
+        {projects.map((project, id) =>
+          <TailCard
+            key={id}
+            photo={project.photo}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- pages/Resume.js
+++ pages/Resume.js
@@ -5,11 +5,6 @@ import React from 'react';
 import styles from '../styles/utils.module.css';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import WorkIcon from '@material-ui/icons/Work';
-import SchoolIcon from '@material-ui/icons/School';
-import StarIcon from '@material-ui/icons/Star';
-import Chip from '@material-ui/core/Chip';
-import Avatar from '@material-ui/core/Avatar';
 
 
 
@@ -31,82 +26,9 @@ export default function FirstPost() {
       <Layout>
         <div className='container mx-auto'>
             <div className='block mt-5 mx-3'>
-              <h1 className='text-5xl font-bold text-center text-gray-800'> About me </h1>
-              <p className='text-lg text-left p-5'>I am a motivated, ambitious, dedicated and enthusiastic individual who can work alone or as part of a team with more than 8 years of account management and business development experience. Able to take initiative, plan efficiently, and think out of the box in order to meet deadlines, and reach company targets. Willing to collaborate on projects to achieve common goals.</p>
+              My Resume
             </div>
-            {/* <div className=' m-5'>
-              <h1>My skills: </h1>
-              <div className='flex flex-col'>
-                {testData.map((item, idx) => (
-                  <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} icon={item.icon} style={item.style} />
-                ))}
-              </div> */}
-              <div className='block mt-5 mx-3 text-center'>
-                <h1 className='text-5xl font-bold text-center text-gray-800'>Work Experience & Education</h1>
-              </div>
-              <VerticalTimeline className={`${styles.verticalcustomeline}`}>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--work text-xl"
-                  contentStyle={{ 
-                    color: 'black'
-                    }}
-                  contentArrowStyle={{ borderRight: '15px solid rgba(74, 85, 104)' }}
-                  date="March 2018 - present"
-                  position={'left'}
-                  iconStyle={{ background: 'black', color: '#fff' }}
-                  icon={<WorkIcon />}
-                >
-                  <h3 className="text-2xl">Market manager</h3>
-                  <h4 className="text-xl">London, UK</h4>
-                  <img src='https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Expedia_Group_logo.svg/1280px-Expedia_Group_logo.svg.png' alt='logo' />
-                  <p>Helping Expedia to develop and maintain a high quality portfolio of partners and property products including strategic partnerships with large groups like IHG, Melia or Hilton. Helping partners to maximise the produc&on potential by using data insights, marketing, distribution and revenue management toolset.</p>
-                </VerticalTimelineElement>
-
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--education"
-                  contentStyle={{ color: 'black' }}
-                  contentArrowStyle={{ borderRight: '15px solid rgba(74, 85, 104)' }}
-                  date="Oct 2019 - January 2020"
-                  position={'right'}
-                  iconStyle={{ background: 'black', color: '#fff' }}
-                  icon={<SchoolIcon />}
-                >
-                  <h3 className="text-2xl m-1">Frontend Development - Udacity</h3>
-                  <h4 className="text-xl m-1">Nanodegree</h4>
-                  <Chip label={'HTML'} />
-                  <Chip label={'CSS'} />
-                  <Chip label={'JAVASCRIPT'} />
-                  <Chip label={'NODE JS'} />
-                  <Chip label={'WEBPACK'} />
-                  <Chip label={'EXPRESS JS'} />
-                  <img src='https://lh3.googleusercontent.com/proxy/dbjMLg6q8peINZ5IVPp5zjnkPDWC3EPF8yaJ55ubdoKzg_VtypmPCid-811gIzZ68zGSlDkK7W6BtNlM3tm1pzgvF9YZ4ZWEHjbz7mfTtbgussxB3j7UMTI2-Gi_' alt='logo' />
-                </VerticalTimelineElement>
-
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--education"
-                  contentStyle={{ color: 'black' }}
-                  contentArrowStyle={{ borderRight: '15px solid rgba(74, 85, 104)' }}
-                  date="June 2019 - September 2019"
-                  position={'left'}
-                  iconStyle={{ background: 'black', color: '#fff' }}
-                  icon={<SchoolIcon />}
-                >
-                  <h3 className="text-2xl m-1">Introduction to Programming - Udacity</h3>
-                  <h4 className="text-xl m-1">Nanodegree</h4>
-                  <Chip label={'HTML'}/>
-                  <Chip label={'CSS'} />
-                  <Chip label={'JAVASCRIPT'} />
-                  <Chip label={'PYTHON'} />
-                  <img src='https://lh3.googleusercontent.com/proxy/dbjMLg6q8peINZ5IVPp5zjnkPDWC3EPF8yaJ55ubdoKzg_VtypmPCid-811gIzZ68zGSlDkK7W6BtNlM3tm1pzgvF9YZ4ZWEHjbz7mfTtbgussxB3j7UMTI2-Gi_' alt='logo' />
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                  icon={<StarIcon />}
-                />
-              </VerticalTimeline>
-            
           </div>
-
       </Layout>
       </>
   );
