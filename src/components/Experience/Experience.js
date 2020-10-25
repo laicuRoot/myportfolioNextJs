c3773c545087947ac4ef66cad8200eac08cce174
laicuRoot <jdblancoes@gmail.com>
2020-10-25 10:39:58 +0000

    Adding MobileLifeCard to Edu and Exp component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,28 @@
 import React,{useState} from 'react'
 import LifeCard from '../LifeCard/LifeCard';
-import MobileLifeCard from '../MobileCard/MobileLifeCard';
+
+const MobileLifeCard = ({ date, title, description, photo }) => {
+  return (
+    <div className="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+      <div className="bg-white rounded-lg overflow-hidden shadow relative">
+        <img className="h-56 w-full object-contain object-center" src={photo} alt="project photo" />
+        <div className="p-4 h-auto md:h-40 lg:h-48">
+          <title href="#" className="block text-green-400 hover:text-green-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+            {title}
+          </title>
+
+          <div className="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+            <p>{description}</p>
+          </div>
+
+          <div className="relative p-1 mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:flex w-full">
+            <p>{date}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const Experience = ({experiences}) => {
 
