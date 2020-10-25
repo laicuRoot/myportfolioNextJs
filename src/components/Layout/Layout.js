5eccc407c7c195254a8a7a760e7670174c1a4c99
laicuRoot <jdblancoes@gmail.com>
2020-10-25 09:02:22 +0000

    change Layout and Nav com for mobile responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <hr />
             <main>
                 <div className='flex flex-col items-start px-5 mx-auto w-full'>
-                  <div className='mt-20 lg:mt-40 lg:m-10'>
+                  <div className='mt-40 lg:mt-40 lg:m-10'>
                 <motion.div initial="hidden" animate="visible" variants={{
                   hidden: {
                     scale: .5,
@@ -45,13 +45,13 @@ export default function Layout({ children, home }) {
                     }
                   },
                 }}>
-                    <h1 className='text-sm md:text-4xl font-bold tracking-tighter leading-tight md:pr-8'> Hello I'm <span className='bg-black text-white p-1 rounded text-sm md:text-4xl lg:p-2'>Jose Blanco</span> </h1>
+                    <h1 className='text-left text-xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8'> Hello I'm <span className='bg-black text-white p-1 rounded text-2xl md:text-4xl lg:p-2'>Jose Blanco</span> </h1>
                   </motion.div>
-                    <h4 className='text-lg md:text-5xl font-bold tracking-tighter leading-tight mt-1'> Front End Developer.</h4>
-                    <p className='text-sm w-full font-medium p-1 mt-1 mb-1 sm:w-1/2 md:text-xl'> I'm a self-taught front-end developer with passion for web development and programming.</p>
+                    <h4 className='text-left text-3xl md:text-5xl font-bold tracking-tighter leading-tight mt-1'> Front End Developer.</h4>
+                    <p className=' text-left text-lg w-full font-medium p-1 mt-1 mb-2 sm:w-1/2 md:text-xl'> I'm a self-taught front-end developer with passion for web development and programming.</p>
                     <div className='flex items-center w-40'>
                       <Link href='/Resume'>
-                    <button className='bg-black text-white rounded-full animate-pulse text-xs py-1 px-2 lg:text-lg'>Find out more</button>
+                    <button className='bg-black text-white rounded-full animate-pulse text-lg py-1 px-2 lg:text-xl'>Find out more</button>
                       </Link>
                     </div>
                   </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,8 +6,8 @@ import navButtons from '../../../buttons';
 
 const NavButton = props => (
   <Link href={props.path} replace>
-    <motion.div className=' block lg:mt-0 font-bold text-black hover:shadow-md mr-2 p-1 rounded ' whileHover={{scale:1.1}} whileTap={{scale:0.8}}>
-      <span className='text-xs lg:text-sm uppercase cursor-pointer lg:flex-grow'>{props.label}</span>
+    <motion.div className=' block p-0 mr-0 lg:mt-0 lg:mr-2 lg:p-1 rounded font-bold text-black hover:shadow-md' whileHover={{scale:1.1}} whileTap={{scale:0.8}}>
+      <span className='text-xs lg:text-sm uppercase cursor-pointer'>{props.label}</span>
     </motion.div>
   </Link>
 );
@@ -18,7 +18,7 @@ export default function Nav(){
     <nav className='flex items-center justify-between flex-wrap p-3 w-full'>
       <div className='flex items-center flex-shrink-0 text-black mr-6'>
         <Link href='/'>
-          <span className='text-xs font-semibold lg:text-xl tracking-tight cursor-pointer'>Jose Blanco</span>
+          <span className='text-sm font-semibold lg:text-xl tracking-tight cursor-pointer'>Jose Blanco</span>
         </Link>
         <button
           className="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -29,10 +29,10 @@ export default function Nav(){
         </button>
       </div>
       <div className={
-        "lg:flex flex-grow items-center justify-end" +
+        "flex-grow items-center lg:justify-end lg:flex" +
         (navbarOpen ? " flex" : " hidden")
         }>
-        <div className='flex justify-end w-full lg:items-center lg:w-auto space-x-4'>
+        <div className='flex justify-center w-full lg:justify-end lg:items-center lg:w-auto space-x-4'>
           {navButtons.map((button, id) => {
             return (
               <NavButton key={id} path={button.path} label={button.label} />
