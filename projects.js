3dd887c7b7018f2e147caa565ade1fb57053130c
laicuRoot <jdblancoes@gmail.com>
2020-10-26 20:44:09 +0000

    added link

--- projects.js
+++ projects.js
@@ -10,19 +10,19 @@ const projects = [
     title: "Yelp App React",
     photo: "/images/yelpsmall.jpg",
     description: 'Find your favourite restaurant. Built with React and Yelp API',
-    link: 'https://github.com/laicuRoot/Weather-App-React',
+    link: 'https://github.com/laicuRoot/Yelp-React-Project',
   },
   {
     title: "Jamming App React",
     photo: "/images/spotify.png",
     description: 'Spotify App using React',
-    link: 'https://github.com/laicuRoot/Weather-App-React',
+    link: 'https://github.com/laicuRoot/Jamming',
   },
   {
     title: "Travel App Website",
     photo: "/images/travel.png",
     description: 'WebApp using data from 3 API\'s',
-    link: 'https://github.com/laicuRoot/Weather-App-React',
+    link: 'https://github.com/laicuRoot/Travel-App-Website',
   }
 ];
 
--- src/components/TailCard/TailCard.js
+++ src/components/TailCard/TailCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function  TailCard ({title, photo, description}){
+function  TailCard ({title, photo, description,link}){
 
   return(
           <div className="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
             <div className="bg-white rounded-lg overflow-hidden shadow relative">
               <img className="h-56 w-full object-cover object-center" src={photo} alt="project photo" />
                 <div className="p-4 h-auto md:h-40 lg:h-48">
-                  <a href="#" className="block text-green-400 hover:text-green-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+                  <a href={link} className="block text-green-400 hover:text-green-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
                     {title}
                   </a>
                   <div className="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
